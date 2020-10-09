f80e74826a03a36e4333289be9cfef6ec24b61a5
SunShogoNakashima <shogo.nakashima@sun-asterisk.com>
2020-10-09 15:10:05 +0900

    variabeized fromAddress, toAddress, mailSubject and mailText

--- mail2/Program.cs
+++ mail2/Program.cs
@@ -13,6 +13,10 @@ namespace mail2
         {
             var host = "smtp.gmail.com";
             var port = 465;
+            var fromAdd = "shougorian3@gmail.com"; //送信元アドレス
+            var toAdd = "shogo.nakashima.1109@gmail.com"; //送信先アドレス
+            var mailSubject = "エラー通知ﾃｽﾄ";
+            var mailText = "お疲れ様です。\r\nエラー通知のテストメールを送信いたしまします。";
 
             using (var smtp = new MailKit.Net.Smtp.SmtpClient())
             {
@@ -22,73 +26,24 @@ namespace mail2
                     //smtp.ServerCertificateValidationCallback = (s, c, h, e) => true;
                     smtp.Connect(host, port, MailKit.Security.SecureSocketOptions.Auto);
                     //認証設定
-                    smtp.Authenticate("shougorian3@gmail.com", "");
+                    smtp.Authenticate(fromAdd, "");
 
                     //送信するメールを作成する
                     var mail = new MimeKit.MimeMessage();
                     var builder = new MimeKit.BodyBuilder();
-                    mail.From.Add(new MimeKit.MailboxAddress("", "shougorian3@gmail.com"));
-                    mail.To.Add(new MimeKit.MailboxAddress("", "shogo.nakashima.1109@gmail.com"));
-                    mail.Subject = "メールタイトル";
+                    mail.From.Add(new MimeKit.MailboxAddress("", fromAdd));
+                    mail.To.Add(new MimeKit.MailboxAddress("", toAdd));
+                    //メールタイトル
+                    mail.Subject = mailSubject;
+                    //メール本文
                     MimeKit.TextPart textPart = new MimeKit.TextPart("Plain");
-                    textPart.Text = "メール本文";
-                    //string filename = "添付ファイルの名前";
-                    //添付ファイルがあった時の処理
-                    //if (!string.IsNullOrEmpty(filename))
-                    //{
-                        //var filePath = Server.MapPath("~/App_Data/doc/" + filename);
-                    //    //var mimeType = MimeKit.MimeTypes.GetMimeType(filePath);
-                    //    //var attachment = new MimeKit.MimePart(mimeType);
-                    //    //using (var file = File.OpenRead(filePath))
-                    //    //{
-                    //    //    attachment.Content = new MimeKit.MimeContent(file);
-                    //        attachment.ContentDisposition = new MimeKit.ContentDisposition();
-                    //        attachment.ContentTransferEncoding = MimeKit.ContentEncoding.Base64;
-                    //        //attachment.FileName = Path.GetFileName(filePath);
-                    //        var multipart = new MimeKit.Multipart("mixed");
-                    //        multipart.Add(textPart);
-                    //        multipart.Add(attachment);
-                    //        mail.Body = multipart;
-                    //        //メールを送信する
-                    //        smtp.Send(mail);
-                    //    }
-                    //}
-                    //else
-                    //{
-                        var multipart = new MimeKit.Multipart("mixed");
-                        multipart.Add(textPart);
-                        mail.Body = multipart;
-                        //メールを送信する
-                        smtp.Send(mail);
-                    //}
-                    ////添付ファイルがあった時の処理
-                    //if (!string.IsNullOrEmpty(filename))
-                    //{
-                    //    var filePath = Server.MapPath("~/App_Data/doc/" + filename);
-                    //    var mimeType = MimeKit.MimeTypes.GetMimeType(filePath);
-                    //    var attachment = new MimeKit.MimePart(mimeType);
-                    //    using (var file = File.OpenRead(filePath))
-                    //    {
-                    //        attachment.Content = new MimeKit.MimeContent(file);
-                    //        attachment.ContentDisposition = new MimeKit.ContentDisposition();
-                    //        attachment.ContentTransferEncoding = MimeKit.ContentEncoding.Base64;
-                    //        attachment.FileName = Path.GetFileName(filePath);
-                    //        var multipart = new MimeKit.Multipart("mixed");
-                    //        multipart.Add(textPart);
-                    //        multipart.Add(attachment);
-                    //        mail.Body = multipart;
-                    //        //メールを送信する
-                    //        smtp.Send(mail);
-                    //    }
-                    //}
-                    //else
-                    //{
-                    //    var multipart = new MimeKit.Multipart("mixed");
-                    //    multipart.Add(textPart);
-                    //    mail.Body = multipart;
-                    //    //メールを送信する
-                    //    smtp.Send(mail);
-                    //}
+                    textPart.Text = mailText;
+
+                    var multipart = new MimeKit.Multipart("mixed");
+                    multipart.Add(textPart);
+                    mail.Body = multipart;
+                    //メールを送信する
+                    smtp.Send(mail);
 
                 }
                 catch (Exception exception)
