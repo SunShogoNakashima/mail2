3c8e5ce00948eda6d33021ccfca3a6e8475af3e3
SunShogoNakashima <shogo.nakashima@sun-asterisk.com>
2020-10-13 11:37:38 +0900

    認証がない場合（fromAddPass=）とある場合で、分岐を実装

--- mail2/Program.cs
+++ mail2/Program.cs
@@ -11,10 +11,21 @@ namespace mail2
     {
         static void Main(string[] args)
         {
+            //認証がある場合
             var host = "smtp.gmail.com";
             var port = 465;
-            var fromAdd = "shougorian3@gmail.com"; //送信元アドレス
+            var fromAdd = "windowsmailingtest@gmail.com"; //送信元アドレス
+            var fromAddPass = "Sun-0036"; //送信元アドレスパスワード
             var toAdd = "shogo.nakashima.1109@gmail.com"; //送信先アドレス
+
+            /* 
+             * //認証がない場合
+            var host = "localhost";
+            var port = 25;
+            var fromAdd = "windowsmailingtest@gmail.com"; //送信元アドレス
+            var fromAddPass = ""; //送信元アドレスパスワード
+            var toAdd = "local@testmail.com"; //送信先アドレス
+            */
             var mailSubject = "エラー通知ﾃｽﾄ";
             var mailText = "お疲れ様です。\r\nエラー通知のテストメールを送信いたしまします。";
 
@@ -22,29 +33,53 @@ namespace mail2
             {
                 try
                 {
-                    //開発用のSMTPサーバが暗号化に対応していないときは、次の行をコメントアウト
-                    //smtp.ServerCertificateValidationCallback = (s, c, h, e) => true;
-                    smtp.Connect(host, port, MailKit.Security.SecureSocketOptions.Auto);
-                    //認証設定
-                    smtp.Authenticate(fromAdd, "");
+                    if (fromAddPass == "")
+                    {
+                        smtp.Connect(host, port, MailKit.Security.SecureSocketOptions.Auto);
+                        //認証設定をコメントアウト
+                        //smtp.Authenticate(fromAdd, fromAddPass);
 
-                    //送信するメールを作成する
-                    var mail = new MimeKit.MimeMessage();
-                    var builder = new MimeKit.BodyBuilder();
-                    mail.From.Add(new MimeKit.MailboxAddress("", fromAdd));
-                    mail.To.Add(new MimeKit.MailboxAddress("", toAdd));
-                    //メールタイトル
-                    mail.Subject = mailSubject;
-                    //メール本文
-                    MimeKit.TextPart textPart = new MimeKit.TextPart("Plain");
-                    textPart.Text = mailText;
+                        //送信するメールを作成する
+                        var mail = new MimeKit.MimeMessage();
+                        var builder = new MimeKit.BodyBuilder();
+                        mail.From.Add(new MimeKit.MailboxAddress("", fromAdd));
+                        mail.To.Add(new MimeKit.MailboxAddress("", toAdd));
+                        //メールタイトル
+                        mail.Subject = mailSubject;
+                        //メール本文
+                        MimeKit.TextPart textPart = new MimeKit.TextPart("Plain");
+                        textPart.Text = mailText;
 
-                    var multipart = new MimeKit.Multipart("mixed");
-                    multipart.Add(textPart);
-                    mail.Body = multipart;
-                    //メールを送信する
-                    smtp.Send(mail);
+                        var multipart = new MimeKit.Multipart("mixed");
+                        multipart.Add(textPart);
+                        mail.Body = multipart;
+                        //メールを送信する
+                        smtp.Send(mail);
 
+                    }
+                    else
+                    {
+                        smtp.Connect(host, port, MailKit.Security.SecureSocketOptions.Auto);
+                        //認証設定
+                        smtp.Authenticate(fromAdd, fromAddPass);
+
+                        //送信するメールを作成する
+                        var mail = new MimeKit.MimeMessage();
+                        var builder = new MimeKit.BodyBuilder();
+                        mail.From.Add(new MimeKit.MailboxAddress("", fromAdd));
+                        mail.To.Add(new MimeKit.MailboxAddress("", toAdd));
+                        //メールタイトル
+                        mail.Subject = mailSubject;
+                        //メール本文
+                        MimeKit.TextPart textPart = new MimeKit.TextPart("Plain");
+                        textPart.Text = mailText;
+
+                        var multipart = new MimeKit.Multipart("mixed");
+                        multipart.Add(textPart);
+                        mail.Body = multipart;
+                        //メールを送信する
+                        smtp.Send(mail);
+                    }
                 }
                 catch (Exception exception)
                 {
