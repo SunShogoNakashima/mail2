e2f8468dad781645d4fa6e6f3e2e59b0f8e6f48e
Shunya TAKANO <71380394+shunya-takano@users.noreply.github.com>
2020-10-14 10:48:37 +0900

    Program.csを更新しました
    変更点は以下です。
    １．メソッドをSendMail.csファイルへ分割
    ２．SMTP認証をtrue or falseで行う形へ変更

--- mail2/Program.cs
+++ mail2/Program.cs
@@ -11,73 +11,22 @@ namespace mail2
     {
         static void Main(string[] args)
         {
-            var info = new Info();
+            // メールを送信する機能全体を別クラスにする（そうしておくと、再利用しやすい、ミナジンのプロジェクトに移植しやすい）
+            var sendMail = new SendMail();
 
             //以下に必要な値を入力
-            info.host = "smtp.gmail.com";//ホスト名
-            info.port = 465;//ポート番号
-            info.fromAdd = "windowsmailingtest@gmail.com"; //送信元アドレス
-            info.fromAddPass = "Sun-0036"; //送信元アドレスパスワード
-            info.toAdd = "shogo.nakashima.1109@gmail.com"; //送信先アドレス
-            info.mailSubject = "エラー通知ﾃｽﾄ";
-            info.mailText = "お疲れ様です。\r\nエラー通知のテストメールを送信いたします。";//メール本文
+            sendMail.Host = "smtp.gmail.com";//ホスト名
+            sendMail.Port = 465;//ポート番号
+            sendMail.FromAdd = "windowsmailingtest@gmail.com"; //送信元アドレス
+            sendMail.FromAddPass = "Sun-0036"; //送信元アドレスパスワード
+            sendMail.ToAdd = "shunyatakano0711@gmail.com"; //送信先アドレス
+            sendMail.MailSubject = "エラー通知ﾃｽﾄ";
+            sendMail.MailText = "お疲れ様です。\r\nエラー通知のテストメールを送信いたします。";//メール本文
+            sendMail.UseSmtpAuth = true; // true=SMTP認証を使用する / false=SMTP認証を使用しない
 
-            var host = info.host;
-            var port = info.port;
-            var fromAdd = info.fromAdd;
-            var fromAddPass = info.fromAddPass;
-            var toAdd = info.toAdd;
-            var mailSubject = info.mailSubject;
-            var mailText = info.mailText;
-
-            using (var smtp = new MailKit.Net.Smtp.SmtpClient())
-            {
-                try
-                {
-
-                    smtp.Connect(host, port, MailKit.Security.SecureSocketOptions.Auto);
-                    
-                    //認証設定
-                    if (fromAddPass != "")
-                    {    
-                        smtp.Authenticate(fromAdd, fromAddPass);
-                    }
-                    var mail = new MimeKit.MimeMessage();
-                    var builder = new MimeKit.BodyBuilder();
-                    mail.From.Add(new MimeKit.MailboxAddress("", fromAdd));
-                    mail.To.Add(new MimeKit.MailboxAddress("", toAdd));
-                    mail.Subject = mailSubject;
-                    MimeKit.TextPart textPart = new MimeKit.TextPart("Plain");
-                    textPart.Text = mailText;
-
-                    var multipart = new MimeKit.Multipart("mixed");
-                    multipart.Add(textPart);
-                    mail.Body = multipart;
-                    //メールを送信する
-                    smtp.Send(mail);
-                }
-                catch (Exception exception)
-                {
-                    Console.WriteLine(exception.Message);
-                }
-                finally
-                {
-                    //SMTPサーバから切断する
-                    smtp.Disconnect(true);
-                }
-            }
+            // メール送信
+            sendMail.Send();
         }
 
-    }
-
-    class Info
-    {
-        public string host { get; set; }
-        public int port { get; set; }
-        public string fromAdd { get; set; }
-        public string fromAddPass { get; set; }
-        public string toAdd { get; set; }
-        public string mailSubject { get; set; }
-        public string mailText { get; set; }
     }
 }
